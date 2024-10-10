69c1fd525e404258ee9d9c163ef21ff878eac30b
Dawid Szemro <dawid.sz@tutanota.com>
2024-10-10 18:10:03 +0200

    refactor: Refactor adapters
    Refactor evolve to use separate functions for each event type. Rename file to follow CRUD naming convention.
    
    Refs: #21

--- src/libs/adapters/projects/state/evolve.ts
+++ src/libs/adapters/projects/state/evolve.ts
@@ -1,4 +1,11 @@
-import { BaseProject, Project, ProjectEvent, ProjectImage } from '@/libs/adapters/projects/models';
+import {
+    BaseProject,
+    Project,
+    ProjectCreatedEvent,
+    ProjectEvent,
+    ProjectImage,
+    ProjectImageUploadedEvent,
+} from '@/libs/adapters/projects/models';
 import { v4 } from 'uuid';
 import { IllegalEventError } from './illegal-event-errors';
 import { isEvolved } from './is-evolved';
@@ -19,45 +26,57 @@ const convertEvent = (event: ProjectEvent): Project['events'][number] => ({
     date: event.data.date,
 });
 
+const handleProjectCreation = (
+    state: Project | BaseProject,
+    event: ProjectCreatedEvent,
+): Project => {
+    if (isEvolved(state))
+        throw new IllegalEventError(
+            `Cannot apply event of type ${event.data.type} to Project with status ${state.status}`,
+        );
+
+    return {
+        ...state,
+        status: 'created',
+        name: event.data.name,
+        description: event.data.description,
+        createdAt: event.data.date,
+        heroImage: defaultProjectImage,
+        images: [],
+        events: [convertEvent(event)],
+    };
+};
+
+const handleImageUpload = (
+    state: Project | BaseProject,
+    event: ProjectImageUploadedEvent,
+): Project => {
+    if (!isEvolved(state)) {
+        throw new IllegalEventError(
+            `Cannot apply event of type ${event.data.type} to not evolved Project`,
+        );
+    }
+
+    if (state.images.some(x => x.uuid === event.data.imageUuid)) {
+        throw new IllegalEventError(`Cannot upload image that already exists in Project`);
+    }
+
+    const newImage = getProjectImage(state, event.data.imageUuid);
+
+    return {
+        ...state,
+        heroImage: event.data.isDefault ? newImage : state.heroImage,
+        images: [...state.images, newImage],
+        events: [...state.events, convertEvent(event)],
+    };
+};
+
 export const evolve = (state: Project | BaseProject, event: ProjectEvent): Project => {
-    const { data } = event;
-
-    switch (data.type) {
+    switch (event.data.type) {
         case 'ProjectCreated':
-            if (isEvolved(state))
-                throw new IllegalEventError(
-                    `Cannot apply event of type ${data.type} to Project with status ${state.status}`,
-                );
-
-            return {
-                ...state,
-                status: 'created',
-                name: data.name,
-                description: data.description,
-                createdAt: data.date,
-                heroImage: { ...defaultProjectImage },
-                images: [],
-                events: [convertEvent(event)],
-            };
+            return handleProjectCreation(state, event as ProjectCreatedEvent);
 
         case 'ProjectImageUploaded':
-            if (!isEvolved(state)) {
-                throw new IllegalEventError(
-                    `Cannot apply event of type ${data.type} to not evolved Project`,
-                );
-            }
-
-            if (state.images.some(x => x.uuid === data.imageUuid)) {
-                throw new IllegalEventError(`Cannot upload image that already exists in Project`);
-            }
-
-            return {
-                ...state,
-                heroImage: data.isDefault
-                    ? getProjectImage(state, data.imageUuid)
-                    : state.heroImage,
-                images: [...state.images, getProjectImage(state, data.imageUuid)],
-                events: [...state.events, convertEvent(event)],
-            };
+            return handleImageUpload(state, event as ProjectImageUploadedEvent);
     }
 };
